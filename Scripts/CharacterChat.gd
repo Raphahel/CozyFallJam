3938bac93251075ede1d906e26119cc4e5e90f9b
Gaspard <gaspard.dama@gmail.com>
2022-09-24 01:50:15 +0200

    1

---
+++ Scripts/CharacterChat.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+
+const PLAYER_SPEED := 250
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(_delta):
+	var velocity = Vector2.ZERO
+
+	# Move RIGHT
+	if Input.is_action_pressed("move_right"):
+		velocity.x = PLAYER_SPEED
+
+	# Move LEFT
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -PLAYER_SPEED
+
+	# Move DOWN
+	if Input.is_action_pressed("move_down"):
+		velocity.y = PLAYER_SPEED
+
+	# Move UP
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -PLAYER_SPEED
+
+	move_and_slide(velocity)
+
+
