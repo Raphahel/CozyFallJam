98e8f7904ddaeed2413b2ba69c7eee7b1da70b83
Gaspard <gaspard.dama@gmail.com>
2022-09-24 10:33:04 +0200

    Changed files structure

---
+++ Scripts/GlobalVariables.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var effet = {e1="soin",e2="poison",e3="sommeil", e4="force", e5="croissance", e6="hydratation", e7="oubli", e8="vision de nuit"}
+
+	
+
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- Scripts/Potion.gd
+++ Scripts/Potion.gd
@@ -5,20 +5,27 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-var effet = {e1="soin",e2="poison",e3="sommeil", e4="force", e5="croissance", e6="hydratation", e7="oubli", e8="vision de nuit"}
-Potion 
+var prix = 0
+var effet = ""
+var nom = "" 
+var superieur = false
+var effets = {e1="soin",e2="poison",e3="sommeil", e4="force", e5="croissance", e6="hydratation", e7="oubli", e8="vision de nuit"}
+
+func _init(var e,var n, var p, var sup):
+	effet = e
+	nom = n
+	prix = p
+	superieur = sup
+
 func concatenation (var effet): 
 	var res
 	res = "Potion de "+ effet
 	return res
-	
 
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
