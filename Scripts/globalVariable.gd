56e67c11da4c0270b7cce4008ff96285e27c566c
Gaspard <gaspard.dama@gmail.com>
2022-09-24 14:57:24 +0200

    smol edit

--- Scripts/globalVariable.gd
+++ Scripts/globalVariable.gd
@@ -1,16 +1,35 @@
 var money
-enum Effects {HEALTH, POISON, STRENGTH, GROWTH, SLEEP, FORGETFULNESS, NIGHT_VISION, HYDRATION}
-var inventaire = {"Blue Mushroom" : 0, "Red Mushroom" : 0, "Green Mushroom" : 0, "Snail" : 0, "Polka Dot Snail" : 0, "Kilted Snail" : 0, "Butterfly" : 0, "Blue Morpho" : 0, "Bubblegum Butterfly" : 0, "Potion of Health" : 0, "Potion of Poison" : 0, "Potion of Strength" : 0, "Potion of Growth" : 0, "Potion of Sleep" : 0, "Potion of Forgetfulness" : 0, "Potion of Night Vision" : 0, "Potion of Hydration" : 0}
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+enum Effects {
+	HEALTH,
+	POISON,
+	STRENGTH,
+	GROWTH,
+	SLEEP,
+	FORGETFULNESS,
+	NIGHT_VISION,
+	HYDRATION
+}
 
-# Called when the node enters the scene tree for the first time.
+var inventaire = {
+	"Blue Mushroom": 0,
+	"Red Mushroom": 0,
+	"Green Mushroom": 0,
+	"Snail" : 0,
+	"Polka Dot Snail": 0,
+	"Kilted Snail": 0,
+	"Butterfly": 0,
+	"Blue Morpho": 0,
+	"Bubblegum Butterfly": 0,
+	"Potion of Health": 0,
+	"Potion of Poison": 0,
+	"Potion of Strength": 0,
+	"Potion of Growth": 0,
+	"Potion of Sleep": 0,
+	"Potion of Forgetfulness": 0,
+	"Potion of Night Vision": 0,
+	"Potion of Hydration": 0
+}
+
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
