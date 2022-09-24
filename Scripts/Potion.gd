5a7fc998b08811260bab5fb2b9b01a0c9397f740
Raphahel <raphael@raphahellpc.localdomain>
2022-09-24 15:12:39 +0200

    Last merge

--- Scripts/Potion.gd
+++ Scripts/Potion.gd
@@ -1,13 +1,7 @@
-extends Node2D
+extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 var prix = 0
 var effet = ""
@@ -25,7 +19,3 @@ func concatenation (var effet):
 	var res
 	res = "Potion de "+ effet
 	return res
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
