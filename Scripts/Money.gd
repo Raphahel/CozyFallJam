a4eabc24475f97cdff5163908e5439d15a0b201e
Gaspard <gaspard.dama@gmail.com>
2022-09-25 23:08:59 +0200

    Enhanced UI

--- Scripts/Money.gd
+++ Scripts/Money.gd
@@ -1,17 +1,9 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
-func _process(delta):
 	$CanvasLayer/NinePatchRect/Money.text = str(globalVariable.money)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _on_Timer_timeout():
+	$CanvasLayer/NinePatchRect/Money.text = str(globalVariable.money)
