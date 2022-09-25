fb2fb7311063eb654154a3ce946143e5125a02fa
Raphahel <raphael@raphahellpc.localdomain>
2022-09-25 10:06:51 +0200

    crafting update

--- Scripts/CraftingScreen.gd
+++ Scripts/CraftingScreen.gd
@@ -2,4 +2,3 @@ extends Control
 
 const INGR = preload("res://Scenes/Ingredients.tscn")
 
-var LockedIngredients = 
--- Scripts/CraftingSystem.gd
+++ Scripts/CraftingSystem.gd
@@ -1,9 +1,44 @@
 extends Control
 
+var namesAlreadyPlaced = []
+
+var isSlot1used = false
+
+var TargetData1 = {
+	"Name" : "",
+	"Effects" : [],
+	"Texture" : Texture
+}
+
+var TargetData2 = {
+	"Name" : "",
+	"Effects" : [],
+	"Texture" : Texture
+}
+
+func can_drop_data(position, data):
+	var canDrop : bool
+	canDrop = typeof(data) == TYPE_DICTIONARY && !data["Name"].match(TargetData1["Name"]) && data["Name"] != TargetData2["Name"]
+	return canDrop
+
+func drop_data(position, data):
+	if(!isSlot1used):
+		TargetData1["Name"] = data["Name"]
+		TargetData1["Effects"] = data["Effects"]
+		TargetData1["Texture"] = data["Texture"]
+		$slot1/Texture.texture = TargetData1["Texture"]
+		isSlot1used = true
+	else:
+		TargetData2["Name"] = data["Name"]
+		TargetData2["Effects"] = data["Effects"]
+		TargetData2["Texture"] = data["Texture"]
+		$slot2/Texture.texture = TargetData2["Texture"]
 
 func _process(delta):
-	var eff1 = $slot1.TargetData["Effects"]
-	var eff2 = $slot2.TargetData["Effects"]
+	namesAlreadyPlaced = [TargetData1["Name"], TargetData2["Name"]]
+	if(Input.action_press("left_click") && TargetData1["Name"] != "none" && TargetData2["Name"] != "none"):
+		var outputPotion = get_blend_effects(TargetData1["Effects"], TargetData2["Effects"])
+
 
 
 func get_blend_effects(var eff1, var eff2):
